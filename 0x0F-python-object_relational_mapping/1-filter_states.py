39356d850b26f02464e0e84d21f81ee9b96069d6
RajaaKacemi <rajaa01.kacemi@gmail.com>
2024-04-20 13:37:39 -0700

    add files

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from the Database"""
+
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    starting with capital letter N
+    Username, password and database names are given as user args
+"""
+
 
 import sys
-from db_conn import connect_db
+import MySQLdb
 
-"""
-    the command line arguments provides the mysql auth credentials
-"""
-_args = sys.argv
 
-if __name__ == "__main__":
-    db = connect_db(_args[1:])
-    cur = db.cursor()
-    cur.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-            )
-    rows = cur.fetchall()
-    for row in rows:
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states\
+                    WHERE name LIKE BINARY 'N%'\
+                    ORDER BY id ASC")
+
+    data = cursor.fetchall()
+
+    for row in data:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
